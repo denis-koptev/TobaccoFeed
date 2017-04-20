2220bd67e8230094b2ab74249c73fcf06e5d4334
Denis Koptev <koptevda@ya.ru>
2017-04-20 10:55:26 +0300

    Tobacco parameters (test mode) added

--- django/tobaccopoisk/tobacco/views.py
+++ django/tobaccopoisk/tobacco/views.py
@@ -7,5 +7,6 @@ from django.http import Http404
 
 def tobacco_view(request, brand, name):
 	tobacco = get_object_or_404(Tobacco, brand=brand, name=name)
-	context = {'brand': brand.title(), 'name':name.title(),}
+	context = {'brand': brand.title(), 'name':name.title(),'description':tobacco.description,
+			   'strength':tobacco.strength, 'taste':tobacco.taste, 'heat':tobacco.heat, 'smoke':tobacco.smoke,}
 	return render(request, 'tobacco/index.html', context)
