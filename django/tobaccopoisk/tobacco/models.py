e41c2c4d94b59560e0b98b295a72ce2801f92a19
japroc <gps_tennis@mail.ru>
2017-04-22 03:44:37 +0300

    [Fix] Now it's possible to leave fields blank in admin panel

--- django/tobaccopoisk/tobacco/models.py
+++ django/tobaccopoisk/tobacco/models.py
@@ -17,6 +17,9 @@ class PathAndRename(object):
 	def __call__(self, instance, filename):
 		ext = filename.split('.')[-1]
 		filename = instance.brand + '_' + instance.name + '.' + ext
+
+		# if some bugs would be detected change to
+		# return os.path.join(self.path, filename)
 		return self.path + filename
 
 path_and_rename = PathAndRename("tobacco/static/tobacco/")
@@ -28,15 +31,15 @@ path_and_rename = PathAndRename("tobacco/static/tobacco/")
 class Tobacco(models.Model):
 	brand = models.CharField(max_length=20)
 	name = models.CharField(max_length=30)
-	release_date = models.DateField(auto_now_add=True, null=True)
-	description = models.TextField(null=True)
-	strength = models.FloatField(null=True)
-	smoke = models.FloatField(null=True)
-	taste = models.FloatField(null=True)
-	heat = models.FloatField(null=True)
-	rating = models.FloatField(null=True)
+	release_date = models.DateField(auto_now_add=True, null=True, blank=True)
+	description = models.TextField(null=True, blank=True)
+	strength = models.FloatField(null=True, blank=True)
+	smoke = models.FloatField(null=True, blank=True)
+	taste = models.FloatField(null=True, blank=True)
+	heat = models.FloatField(null=True, blank=True)
+	rating = models.FloatField(null=True, blank=True)
 	# create image name according to object brand and model
-	image = models.ImageField(null=True, upload_to=path_and_rename)
+	image = models.ImageField(null=True, blank=True, upload_to=path_and_rename)
 
 	def __str__(self):
 		return self.brand.title() + ' ' +self.name.title()
