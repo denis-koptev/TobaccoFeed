1410a2ea47954220e82d44bc1f964ff12b54b561
Denis Koptev <koptevda@ya.ru>
2017-04-22 22:21:24 +0300

    Tobacco names python reparsing

--- django/tobaccopoisk/tobacco/views.py
+++ django/tobaccopoisk/tobacco/views.py
@@ -6,6 +6,17 @@ def image_url_handler(url):
 	idx = url.find('/static/')
 	return url[idx:]
 
+def space_to_underscore(str):
+	return str.replace(' ', '_')
+
+def underscore_to_space(str):
+	return str.replace('_', ' ')
+
+def hyphen_to_underscore(str):
+	return str.replace('-','_')
+
+def underscore_to_hyphen(str):
+	return str.replace('_','-')
 
 def tobacco_view(request, brand, name):
 	try:
@@ -13,8 +24,8 @@ def tobacco_view(request, brand, name):
 	except Tobacco.DoesNotExist:
 		return render(request, 'error_404.html', {})
 
-	context = {'brand': brand.title(), 
-	           'name': name.title(), 
+	context = {'brand': underscore_to_space(brand.title()), 
+	           'name': underscore_to_space(name.title()), 
 	           'tobacco': tobacco, 
 	           'image': image_url_handler(tobacco.image.name)}
 
