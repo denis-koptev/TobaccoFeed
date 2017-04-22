c4f0186e0d303655d66ed55e73b8b52865be7c6a
japroc <gps_tennis@mail.ru>
2017-04-22 03:25:35 +0300

    [Fix] Fixed ImageField for tobacco model

--- django/tobaccopoisk/tobacco/models.py
+++ django/tobaccopoisk/tobacco/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# ----------------
+# Start of routine
+# ----------------
+# This routine is for handling dynamic ImageField
+# According to https://code.djangoproject.com/ticket/22999
+
+from django.utils.deconstruct import deconstructible
+
+@deconstructible
+class PathAndRename(object):
+
+	def __init__(self, sub_path):
+		self.path = sub_path
+
+	def __call__(self, instance, filename):
+		ext = filename.split('.')[-1]
+		filename = instance.brand + '_' + instance.name + '.' + ext
+		return self.path + filename
+
+path_and_rename = PathAndRename("tobacco/static/tobacco/")
+
+# --------------
+# End of routine
+# --------------
 
 class Tobacco(models.Model):
 	brand = models.CharField(max_length=20)
@@ -12,4 +35,4 @@ class Tobacco(models.Model):
 	taste = models.FloatField(null=True)
 	heat = models.FloatField(null=True)
 	rating = models.FloatField(null=True)
-	image = models.ImageField(null=True)
+	image = models.ImageField(null=True, upload_to=path_and_rename)
