2ac3bcd56f26bd9b43daf62af01398e4e9871c92
denis-koptev <koptevda@ya.ru>
2017-04-26 04:30:49 +0300

    SEARCH ENGINE CREATED

--- django/tobaccopoisk/search_page/views.py
+++ django/tobaccopoisk/search_page/views.py
@@ -1,6 +1,8 @@
 import json
 from django.http import HttpResponse
 from tobacco.models import Tobacco
+from tobacco.views import tobacco_view
+from django.shortcuts import redirect
 
 def image_url_handler(url):
 	idx = url.find('/static/')
@@ -21,6 +23,43 @@ def search(request):
 		return HttpResponse("{}".format(json.dumps({"data": []}, ensure_ascii=False)))
 
 	q = request.GET.get('q')
+	q = q.replace(' ', '').replace('-', '').replace('_', '')
+
 	data = [to_dict(inst) for inst in insts]
 
-	return HttpResponse("{}".format(json.dumps({"data": data}, ensure_ascii=False)))
+	# PIZDATIY CODE STARTED
+
+	filtered = []
+
+	for item in data:
+
+		ident = item["brand"] + item["name"]
+		ident = ident.replace(' ', '').replace('-', '').replace('_', '')
+		len_ident = len(ident)
+
+		loc_q = q
+
+		eqs = 0
+		for letter in loc_q:
+			idx = ident.find(letter)
+			if idx != -1:
+				ident = ident[:idx] + ident[idx+1:]
+				q_idx = loc_q.find(letter)
+				loc_q = loc_q[:q_idx] + loc_q[q_idx+1:]
+				eqs = eqs + 1
+
+		# SUKAAAA ETO TAKOOOY PIZDATYI REDIRECT!!!! YA KON4IL!!!
+		if eqs / len_ident == 1:
+			return redirect("/" + item["brand"].lower().replace(' ', '_') + "/" + item["name"].lower().replace(' ', '_'))
+			#return redirect("/afzal/pan-raas")
+
+		# Else make filtration using coefficient calculated with user string
+		if eqs / len(q) >= 0.7:
+			item["coeff"] = eqs / len(q)
+			filtered.append(item)
+
+			print(item["brand"] + ' ' + item["name"] + ' ' + ident + ' ' + str(eqs))
+
+		filtered = sorted(filtered, key=lambda k: k['coeff'], reverse = True)
+
+	return HttpResponse("{}".format(json.dumps({"data": filtered}, ensure_ascii=False)))
--- django/tobaccopoisk/tobacco/urls.py
+++ django/tobaccopoisk/tobacco/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<brand>[0-9a-z-_]+)/(?P<name>[0-9a-z-_]+)([/]?)$', views.tobacco_view, name='tobacco_view'),
+    url(r'^(?P<brand>[0-9A-Za-z-_]+)/(?P<name>[0-9A-Za-z-_]+)([/]?)$', views.tobacco_view, name='tobacco_view'),
 ]
