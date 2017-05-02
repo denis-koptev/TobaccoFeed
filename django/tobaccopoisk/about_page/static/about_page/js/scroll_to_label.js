ac1362b7f2708778dffdbb6168e230ef8006b60b
slooowepoke <natasha@osokina.com>
2017-05-02 04:01:40 +0300

    Main page developing started

--- django/tobaccopoisk/about_page/static/about_page/js/scroll_to_label.js
+++ django/tobaccopoisk/about_page/static/about_page/js/scroll_to_label.js
@@ -3,6 +3,6 @@
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
+        $('body,html').animate({scrollTop: top}, 800);
     });
 });
--- django/tobaccopoisk/main_page/views.py
+++ django/tobaccopoisk/main_page/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from tobacco.models import Tobacco
+from search_page.engine import to_dict
+
+def get_last(count):
+	try:
+		last = Tobacco.objects.all().order_by("-id").values_list('brand', 'name', 'image')[:5]
+
+	except Tobacco.DoesNotExist:
+		return []
+	return [to_dict(rec) for rec in last]
 
 def main(request):
-    return render(request, 'main_page/main_page.html', {})
+	context = {'latest' : get_last(5)}
+	return render(request, 'main_page/main_page.html', context)
