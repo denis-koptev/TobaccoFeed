362c80b037872312354f106f5d59805cb20f0d0d
denis-koptev <koptevda@ya.ru>
2017-05-04 00:47:21 +0300

    Slider (jQuery) on about page fixed

--- django/tobaccopoisk/about_page/static/about_page/js/scroll_to_label.js
+++ django/tobaccopoisk/about_page/static/about_page/js/scroll_to_label.js
@@ -1,8 +1,12 @@
  $(document).ready(function(){
     $("#sections").on("click","a", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 800);
+        if ($(this).attr('href').indexOf('#') >= 0) {
+	        var id  = $(this).attr('href'),
+	            top = $(id).offset().top;
+	        $('body,html').animate({scrollTop: top}, 800);
+        } else {
+        	location.href = $(this).attr('href');
+    	}
     });
 });
